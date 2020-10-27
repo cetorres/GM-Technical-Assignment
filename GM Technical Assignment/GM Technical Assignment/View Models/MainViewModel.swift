8b757a454af0f2f84b87a8ace4a9ebfaed70ef92
Carlos Torres <carlos.torres@cacira.com>
2020-10-26 22:45:06 -0600

    Added comments to the MainViewModel class.

--- GM Technical Assignment/GM Technical Assignment/View Models/MainViewModel.swift
+++ GM Technical Assignment/GM Technical Assignment/View Models/MainViewModel.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+/// The view model delegate that is used to communicate data between
+/// the view model class and the view (MainViewController) class.
 protocol MainViewModelDelegate: AnyObject {
     func loadViewDataError(error: String)
     func viewDataLoaded()
@@ -28,11 +30,14 @@ class MainViewModel {
         CommitService.shared.getCommits { [weak self] result in
             switch result {
             case .success(let commitInfoList):
+                // Set the list of commits
                 self?.commitInfoList = commitInfoList
+                // Call the delegate method to update the UI on the view main thread.
                 DispatchQueue.main.async {
                     self?.delegate?.viewDataLoaded()
                 }
             case .failure(let error):
+                // Call the delegate method to show an error message on the UI on the view main thread.
                 DispatchQueue.main.async {
                     self?.delegate?.loadViewDataError(error: "Could not load data. \(error.localizedDescription)")
                 }
