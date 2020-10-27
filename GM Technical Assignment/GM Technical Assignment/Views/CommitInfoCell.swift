7bee805662ff21f139e6b9bde27b1b3504c5aa6a
Carlos Torres <carlos.torres@cacira.com>
2020-10-26 22:17:01 -0600

    Added a custom table view cell called CommitInfoCell to show the author name, commit hash and commit message. Added the storyboard layout and the class associated to it.

---
+++ GM Technical Assignment/GM Technical Assignment/Views/CommitInfoCell.swift
@@ -0,0 +1,22 @@
+//
+//  CommitInfoCell.swift
+//  GM Technical Assignment
+//
+//  Created by Carlos Torres on 10/26/20.
+//
+
+import UIKit
+
+class CommitInfoCell: UITableViewCell {
+
+    @IBOutlet weak var txtAuthorName: UILabel!
+    @IBOutlet weak var txtCommitHash: UILabel!
+    @IBOutlet weak var txtCommitMessage: UILabel!
+
+    func setData(commitInfo: CommitInfo) {
+        txtAuthorName.text = commitInfo.commit.author.name
+        txtCommitHash.text = commitInfo.sha
+        txtCommitMessage.text = commitInfo.commit.message
+    }
+
+}
