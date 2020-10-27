71153ec815f606c60610634401ae834c0c6316fb
Carlos Torres <carlos.torres@cacira.com>
2020-10-26 22:46:38 -0600

    Added comments to the CommitInfoCell class.

--- GM Technical Assignment/GM Technical Assignment/Views/CommitInfoCell.swift
+++ GM Technical Assignment/GM Technical Assignment/Views/CommitInfoCell.swift
@@ -13,6 +13,7 @@ class CommitInfoCell: UITableViewCell {
     @IBOutlet weak var txtCommitHash: UILabel!
     @IBOutlet weak var txtCommitMessage: UILabel!
 
+    // Set the data of the cell from the CommitInfo object
     func setData(commitInfo: CommitInfo) {
         txtAuthorName.text = commitInfo.commit.author.name
         txtCommitHash.text = commitInfo.sha
