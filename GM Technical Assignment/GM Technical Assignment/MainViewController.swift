50acccd5f0d3439875cbc45f2cdf9a2acca2ac1e
Carlos Torres <carlos.torres@cacira.com>
2020-10-26 19:04:42 -0600

    Added initial basic project for a native iOS app using Swift, Storyboard, UIKit.

---
+++ GM Technical Assignment/GM Technical Assignment/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GM Technical Assignment
+//
+//  Created by Carlos Torres on 10/26/20.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
